63f7d5c26a7041889232900e5af7dcceec0f3e2d
Asima Chowdhury <67515830+asima-chowdhury@users.noreply.github.com>
2022-07-05 13:09:30 +0600

    loaded with intial review

--- 03-reviews/app.js
+++ 03-reviews/app.js
@@ -1,5 +1,5 @@
 // local reviews data
-const reviews = [ 
+const reviews = [
   {
     id: 1,
     name: "susan smith",
@@ -37,3 +37,26 @@ const reviews = [
       "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
   },
 ];
+
+
+// select items
+const img = document.getElementById('person-img');
+const author = document.getElementById('author');
+const job = document.getElementById('job');
+const info = document.getElementById('info');
+
+const prevBtn = document.querySelector('.prev-btn');
+const nextBtn = document.querySelector('.next-btn');
+const randomBtn = document.querySelector('.random-btn');
+
+// set starting item
+let currentItem = 0;
+
+// load initial item
+window.addEventListener('DOMContentLoaded', function () {
+  const item = reviews[currentItem];
+  img.src = item.img;
+  author.textContent = item.name;
+  job.textContent = item.job;
+  info.textContent = item.text;
+})
