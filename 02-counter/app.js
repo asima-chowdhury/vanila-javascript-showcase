2421f6fd963a41842fbf075952fc1504a8d1887a
Asima Chowdhury <67515830+asima-chowdhury@users.noreply.github.com>
2022-07-05 11:44:38 +0600

    counter js done

---
+++ 02-counter/app.js
@@ -0,0 +1,36 @@
+// Set initial count
+let count = 0;
+
+// Select value and buttons
+const value = document.querySelector('#value');
+const btns = document.querySelectorAll('.btn');
+
+console.log(btns);
+
+btns.forEach(function (btn) {
+    btn.addEventListener('click', function (e) {
+        console.log(e.currentTarget.classList);
+        const styles = e.currentTarget.classList;
+
+        if (styles.contains('decrease')) {
+            count--;
+        }
+        else if (styles.contains('increase')) {
+            count++;
+        }
+        else {
+            count = 0;
+        }
+        if (count > 0) {
+            value.style.color = 'green';
+        }
+        if (count < 0) {
+            value.style.color = 'red';
+        }
+
+        if (count === 0) {
+            value.style.color = '#222222';
+        }
+        value.textContent = count;
+    })
+})
