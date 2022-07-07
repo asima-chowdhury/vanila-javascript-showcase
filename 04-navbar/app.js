5b44aaacecbfed67842c3950ce8ee90a7d63909d
Asima Chowdhury <67515830+asima-chowdhury@users.noreply.github.com>
2022-07-07 10:20:21 +0600

    navbar without toggle class

--- 04-navbar/app.js
+++ 04-navbar/app.js
@@ -3,3 +3,19 @@
 // add - add class
 // remove - remove class
 // toggle - toggles class 
+
+const navToggle = document.querySelector(".nav-toggle");
+const links = document.querySelector(".links");
+
+navToggle.addEventListener("click", function() {
+    // console.log(links.classList); //output: DOMTokenList ['links', value: 'links']
+    // console.log(links.classList.contains('random')); //output: false
+    // console.log(links.classList.contains('links')); //output: true
+
+    if(links.classList.contains('show-links')){
+        links.classList.remove('show-links');
+    }
+    else{
+        links.classList.add('show-links');
+    }
+});
