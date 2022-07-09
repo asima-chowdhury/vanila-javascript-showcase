c45ecde5e2fa3b235e11c9e26df61b4be4d33dbf
Asima Chowdhury <67515830+asima-chowdhury@users.noreply.github.com>
2022-07-09 20:38:06 +0600

    show question by traversing the dom

--- 07-questions/app.js
+++ 07-questions/app.js
@@ -1,3 +1,14 @@
 //using selectors inside the element
 // traversing the dom
  
+const btns = document.querySelectorAll(".question-btn");
+
+btns.forEach(function(btn) {
+    btn.addEventListener('click', function(e) {
+        // console.log(e.currentTarget.parentElement);
+
+        const question = e.currentTarget.parentElement.parentElement;
+
+        question.classList.toggle('show-text')
+    });
+});
