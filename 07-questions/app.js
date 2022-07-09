f1f759ff94d39e6ad261f44efe6fcc18abf77b8f
Asima Chowdhury <67515830+asima-chowdhury@users.noreply.github.com>
2022-07-09 21:03:50 +0600

    Q & A project done

--- 07-questions/app.js
+++ 07-questions/app.js
@@ -1,14 +1,36 @@
 //using selectors inside the element
-// traversing the dom
- 
-const btns = document.querySelectorAll(".question-btn");
 
-btns.forEach(function(btn) {
-    btn.addEventListener('click', function(e) {
-        // console.log(e.currentTarget.parentElement);
+const questions = document.querySelectorAll('.question');
 
-        const question = e.currentTarget.parentElement.parentElement;
+questions.forEach(function(question) {
+    // console.log(question);
 
-        question.classList.toggle('show-text')
+    const btn = question.querySelector('.question-btn');
+    // console.log(btn);
+
+    btn.addEventListener('click', function() {
+        
+        questions.forEach(function(item) {
+            console.log(item);
+            if(item !== question) {
+                item.classList.remove('show-text');
+            }
+        })
+        question.classList.toggle('show-text');
     });
 });
+
+
+// traversing the dom
+ 
+// const btns = document.querySelectorAll(".question-btn");
+
+// btns.forEach(function(btn) {
+//     btn.addEventListener('click', function(e) {
+//         // console.log(e.currentTarget.parentElement);
+
+//         const question = e.currentTarget.parentElement.parentElement;
+
+//         question.classList.toggle('show-text')
+//     });
+// });
