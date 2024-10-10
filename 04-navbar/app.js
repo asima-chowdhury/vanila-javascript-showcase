f23973312991cf00c7b76f550c00a53fae13124a
asima-chowdhury <asimachowdhury.lu@gmail.com>
2024-10-10 18:22:47 +0600

    project-4 navbar- code indentation fixed

--- 04-navbar/app.js
+++ 04-navbar/app.js
@@ -2,22 +2,22 @@
 // contains - checks classList for specific class
 // add - add class
 // remove - remove class
-// toggle - toggles class 
+// toggle - toggles class
 
 const navToggle = document.querySelector(".nav-toggle");
 const links = document.querySelector(".links");
 
-navToggle.addEventListener("click", function() {
-    // console.log(links.classList); //output: DOMTokenList ['links', value: 'links']
-    // console.log(links.classList.contains('random')); //output: false
-    // console.log(links.classList.contains('links')); //output: true
+navToggle.addEventListener("click", function () {
+  // console.log(links.classList); //output: DOMTokenList ['links', value: 'links']
+  // console.log(links.classList.contains('random')); //output: false
+  // console.log(links.classList.contains('links')); //output: true
 
-    // if(links.classList.contains('show-links')){
-    //     links.classList.remove('show-links');
-    // }
-    // else{
-    //     links.classList.add('show-links');
-    // }
+  // if(links.classList.contains('show-links')){
+  //     links.classList.remove('show-links');
+  // }
+  // else{
+  //     links.classList.add('show-links');
+  // }
 
-    links.classList.toggle('show-links');
+  links.classList.toggle("show-links");
 });
