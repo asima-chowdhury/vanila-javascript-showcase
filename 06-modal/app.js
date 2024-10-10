9608975c0a565ce95ef29f7bd7026cd0c1571c20
asima-chowdhury <asimachowdhury.lu@gmail.com>
2024-10-10 18:28:57 +0600

    project-6 modal code indentation fixed

--- 06-modal/app.js
+++ 06-modal/app.js
@@ -4,22 +4,21 @@
 // when user clicks modal-btn add .open-modal to modal-overlay
 // when user clicks close-btn remove .open-modal from modal-overlay
 
-const modalBtn = document.querySelector('.modal-btn');
-const modalOverlay = document.querySelector('.modal-overlay');
-const closeBtn = document.querySelector('.close-btn');
+const modalBtn = document.querySelector(".modal-btn");
+const modalOverlay = document.querySelector(".modal-overlay");
+const closeBtn = document.querySelector(".close-btn");
 
-modalBtn.addEventListener('click', function(){
-    // console.log(modalBtn.classList); // DOMTokenList(2) ['btn', 'modal-btn', value: 'btn modal-btn']
-    // console.log(modalOverlay.classList); // DOMTokenList ['modal-overlay', value: 'modal-overlay']
-    
-    if(modalOverlay.classList.contains('open-modal')){
-        modalOverlay.classList.remove('open-modal');
-    }
-    else{
-        modalOverlay.classList.add('open-modal');
-    }
+modalBtn.addEventListener("click", function () {
+  // console.log(modalBtn.classList); // DOMTokenList(2) ['btn', 'modal-btn', value: 'btn modal-btn']
+  // console.log(modalOverlay.classList); // DOMTokenList ['modal-overlay', value: 'modal-overlay']
+
+  if (modalOverlay.classList.contains("open-modal")) {
+    modalOverlay.classList.remove("open-modal");
+  } else {
+    modalOverlay.classList.add("open-modal");
+  }
 });
 
-closeBtn.addEventListener('click', function(){
-    modalOverlay.classList.remove('open-modal')
+closeBtn.addEventListener("click", function () {
+  modalOverlay.classList.remove("open-modal");
 });
